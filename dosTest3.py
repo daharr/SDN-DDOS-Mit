1c8d7ce735305ff6b21984db947ebcb2b27a9f02
daharr <daharr1@umbc.edu>
2017-03-27 14:13:20 -0700

    added the second command for mixed traffic

--- dosTest3.py
+++ dosTest3.py
@@ -65,6 +65,7 @@ def dosSim():
     ##en network
     net.stop()
 
+##normal traffic command
 def nortrafcmd( self, line ):
     "this command executes the scripts for traffic generation"
     net = self.mn
@@ -109,7 +110,55 @@ def nortrafcmd( self, line ):
     h9.cmd('/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGSend/ITGSend -T TCP -a 10.0.0.19 -c 200 -C 1 -t 150000 -l sender9.log -x receiver9.log &')
     h20.cmd( '/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGRecv/ITGRecv &' )
     h10.cmd('/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGSend/ITGSend -T TCP -a 10.0.0.20 -c 200 -C 1 -t 150000 -l sender10.log -x receiver10.log &')
+
+##mixed traffic
+def mixtrafcmd( self, line ):
+    "this command executes the scripts for traffic generation"
+    net = self.mn
+    h1 = net.get('h1')
+    h2 = net.get('h2')
+    h3 = net.get('h3')
+    h4 = net.get('h4')
+    h5 = net.get('h5')
+    h6 = net.get('h6')
+    h7 = net.get('h7')
+    h8 = net.get('h8')
+    h9 = net.get('h9')
+    h10 = net.get('h10')
+    h11 = net.get('h11')
+    h12 = net.get('h12')
+    h13 = net.get('h13')
+    h14 = net.get('h14')
+    h15 = net.get('h15')
+    h16 = net.get('h16')
+    h17 = net.get('h17')
+    h18 = net.get('h18')
+    h19 = net.get('h19')
+    h20 = net.get('h20')
+    # send normal traffic through system
+    h11.cmd( '/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGRecv/ITGRecv &' )
+    h1.cmd('/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGSend/ITGSend -T TCP -a 10.0.0.11 -c 200 -C 1 -t 150000 -l sender1.log -x receiver1.log &')
+    h12.cmd( '/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGRecv/ITGRecv &' )
+    h2.cmd('/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGSend/ITGSend -T TCP -a 10.0.0.12 -c 200 -C 1 -t 150000 -l sender2.log -x receiver2.log &')
+    h13.cmd( '/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGRecv/ITGRecv &' )
+    h3.cmd('/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGSend/ITGSend -T TCP -a 10.0.0.13 -c 300 -C 10 -t 150000 -l sender3.log -x receiver3.log &')
+    h14.cmd( '/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGRecv/ITGRecv &' )
+    h4.cmd('/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGSend/ITGSend -T TCP -a 10.0.0.14 -c 200 -C 1 -t 150000 -l sender4.log -x receiver4.log &')
+    h15.cmd( '/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGRecv/ITGRecv &' )
+    h5.cmd('/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGSend/ITGSend -T TCP -a 10.0.0.15 -c 200 -C 1 -t 150000 -l sender5.log -x receiver5.log &')
+    h16.cmd( '/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGRecv/ITGRecv &' )
+    h6.cmd('/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGSend/ITGSend -T TCP -a 10.0.0.16 -c 300 -C 10 -t 150000 -l sender6.log -x receiver6.log &')
+    h17.cmd( '/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGRecv/ITGRecv &' )
+    h7.cmd('/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGSend/ITGSend -T TCP -a 10.0.0.17 -c 200 -C 1 -t 150000 -l sender7.log -x receiver7.log &')
+    h18.cmd( '/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGRecv/ITGRecv &' )
+    h8.cmd('/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGSend/ITGSend -T TCP -a 10.0.0.18 -c 200 -C 1 -t 150000 -l sender8.log -x receiver8.log &')
+    h19.cmd( '/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGRecv/ITGRecv &' )
+    h9.cmd('/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGSend/ITGSend -T TCP -a 10.0.0.19 -c 300 -C 10 -t 150000 -l sender9.log -x receiver9.log &')
+    h20.cmd( '/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGRecv/ITGRecv &' )
+    h10.cmd('/home/ubuntu/D-ITG-2.8.1-r1023/src/ITGSend/ITGSend -T TCP -a 10.0.0.20 -c 200 -C 1 -t 150000 -l sender10.log -x receiver10.log &')
+
 CLI.do_nortrafcmd = nortrafcmd
+CLI.do_mixtrafcmd = mixtrafcmd
 
 if __name__ == '__main__':
     dosSim()
